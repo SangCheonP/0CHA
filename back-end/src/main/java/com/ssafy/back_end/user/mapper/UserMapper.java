b270641b3b6d2ebfe4fe41e3e532ba4d1dc9d647
Park SangCheon <pswlove38@naver.com>
2024-07-26 01:36:21 +0900

    Build--빌드 타입 변경
    jar -> bootJar
    
    
    Former-commit-id: 715d929ce9ffbdb7f4a5dd6781ab77618fcdbf24

--- back-end/src/main/java/com/ssafy/back_end/user/mapper/UserMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/user/mapper/UserMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16155f710215fec189bc0f6b1d719acba3b3775cbe820fe495fe09e15a110db2
-size 239
+oid sha256:71d94cc609522d94cbbb99095836fd94612ed3c85c3452bc3efa939e09287a1d
+size 228
