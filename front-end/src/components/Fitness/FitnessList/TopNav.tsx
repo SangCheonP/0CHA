e438071743821860928c65fd84a2585bad22dc73
JinY <wlsdud6221@naver.com>
2024-07-29 03:28:05 +0900

    design - 운동리스트 UI
    Former-commit-id: 8083d328619710fde49417d03a391d81d06c5587

---
+++ front-end/src/components/Fitness/FitnessList/TopNav.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b53b78ff88fceb31542d146591f813a343185b191a0f1b6c273d66f29dea4d14
+size 1046
---
+++ front-end/src/pages/Fitness/FitnessListPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6768d110996e7892b078d47e6e2fe504b03323ce538d3bb122ac7f8469d5e394
+size 1538
