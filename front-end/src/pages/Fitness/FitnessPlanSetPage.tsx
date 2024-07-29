432e0c8940d909bd25d560acb08b49dbeaabe452
JinY <wlsdud6221@naver.com>
2024-07-29 13:13:28 +0900

    design - border 제거 및 nav 배경색 추가
    Former-commit-id: 2c2af01958d159425c1aaab3b9d9171c25f8bdbb

--- front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
+++ front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29e64b0b95615b3375f8241a68eddda272b69795f9d4427123c23dcd9b3e8bec
-size 2222
+oid sha256:aee8cf91caace4162742770dd04a59ce88d681495391ddb24db008e5cc26c05d
+size 2145
