e93396826b9c11f01e0092c13a35591f5ba99188
JinY <wlsdud6221@naver.com>
2024-07-29 12:17:59 +0900

    design - 공통컴포넌트 추가 및 수정
    Former-commit-id: 98d25efb5a46065c234295847990225dfa2f32ae

--- front-end/src/components/Common/IconSvg.tsx
+++ front-end/src/components/Common/IconSvg.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a85c32a05324a809702c54886f08483e21bba5093a612c7296204fdfd5d49054
-size 335
+oid sha256:82c0561ce54a065f3ac1c20466d506335bd4322e13b04ae296af3245a1cebab8
+size 512
