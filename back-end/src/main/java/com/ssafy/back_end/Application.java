00191784d92827760fe92b629ad577eff02127ea
Ubuntu <ubuntu@ip-172-26-14-213.ap-northeast-2.compute.internal>
2024-07-29 07:08:55 +0000

    Update .gitignore to exclude data directories for mysql and jenkins
    Former-commit-id: 7c8b5fe434564c132b3cb72725eb686b51762b2f

--- back-end/src/main/java/com/ssafy/back_end/Application.java
+++ back-end/src/main/java/com/ssafy/back_end/Application.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:851b17d7e1e3a1f24381cb2f506de1b5cf0a18e23c3d866ee1a00fbb0e1412ff
-size 311
+oid sha256:4fb9effcb7b99a2668c8fe32722840353b543238505f4356ca2f1d248d4ea504
+size 324
