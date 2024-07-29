7c12297fc305d8a66816c5f21fbb4ea3d764fe42
JinY <wlsdud6221@naver.com>
2024-07-29 17:56:56 +0900

    design - 루틴상세설정, 운동 상세정보 UI
    Former-commit-id: c1671abed79ced83f243a18957a6cd132a5085f1

---
+++ front-end/src/pages/Fitness/FitnessDetailPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:44702fe128870e634f2d28c9874218713f8f719c56a9df9806a2708a1eb2f359
+size 1986
--- front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
+++ front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aee8cf91caace4162742770dd04a59ce88d681495391ddb24db008e5cc26c05d
-size 2145
+oid sha256:830574e0e5a443efd76bdfdc9c8e59ce16922fb1a94215eb3ac68af3353a160c
+size 2408
