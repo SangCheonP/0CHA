4d832d446d7ded4516c66e6e0609c0e81f5a5ed5
Park SangCheon <pswlove38@naver.com>
2024-07-30 14:31:39 +0900

    feat-psc-Modify Port
    Former-commit-id: dbfa8b864ed61a0f3dd582767a530d2c2b9caa57

--- back-end/src/main/java/com/ssafy/back_end/user/model/UserDto.java
+++ back-end/src/main/java/com/ssafy/back_end/user/model/UserDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb248fbc9d80130b9c544ff15a7b634c91fbf453eaf98289a8c792d7b01b2a3d
-size 198
+oid sha256:268cdd795150424f242150d518ed6b0d18fd31be719fa8037e87e036cd8be282
+size 190
