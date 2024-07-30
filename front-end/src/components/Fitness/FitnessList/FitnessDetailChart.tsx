9141de9e1718d78bda22a7f4b1eaf015da6f7e40
JinY <wlsdud6221@naver.com>
2024-07-30 13:24:52 +0900

    fix - Header fixed처리에 관한 padding값 변경
    Former-commit-id: 4780496ab4fea52b3478797b8e76306e1def5203

--- front-end/src/components/Fitness/FitnessList/FitnessDetailChart.tsx
+++ front-end/src/components/Fitness/FitnessList/FitnessDetailChart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9195b4c687526399c697ec584b50ca12c6de60c1a92c2960360cbdf95cf63ffa
-size 2059
+oid sha256:883c623e5d6d1c9884903978104311da2e83df1b4796bf6c96197cc10ae00694
+size 2333
---
+++ front-end/src/pages/Fitness/FitnessLoutineListPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:53354f8004214c6ade3a3ee13e129fef5488047d7c0cf35a22348e1a2e218772
+size 741
