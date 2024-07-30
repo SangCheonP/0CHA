57c45c06ae2dab249525638862960a06c1ec487a
JinY <wlsdud6221@naver.com>
2024-07-30 13:32:21 +0900

    style - Button, Header, Image 스타일 변경 및 추가
    Former-commit-id: 9c421b7649e5d688098662211f65b3cee9328a8c

--- front-end/src/components/Common/Header.tsx
+++ front-end/src/components/Common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b676fbd8052963ce09e386d420e13e6d929a44fd48b48dd6c5cde573d7c3092
-size 1064
+oid sha256:2064c1c3fa71fba749eebec842474458c836dc49abebf5bb59390308a17bcbb7
+size 1106
