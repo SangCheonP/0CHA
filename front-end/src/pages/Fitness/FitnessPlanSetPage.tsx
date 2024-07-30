8a4e218a3cb76a047b22ef0ce51b9813815dab28
JinY <wlsdud6221@naver.com>
2024-07-31 03:25:12 +0900

    style - 캘린더 및 모달 스타일 수정
    Former-commit-id: 54fd3ca906cd62c51c50fa0bb6b4ba1d5e621088

--- front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
+++ front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa5a7a21d26cb5f74f0591e2f6331e1c229949152a76a5b1e61b7b531d88ee64
-size 3043
+oid sha256:06f0de3c0d7b9672990bbf5e452239115beebda72e399b957845765244d88b2b
+size 3140
