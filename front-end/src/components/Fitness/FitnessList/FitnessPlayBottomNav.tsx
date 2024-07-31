ffa11376385d8a102fc936c39214f046d57315cb
JinY <wlsdud6221@naver.com>
2024-07-31 17:51:36 +0900

    init - 운동 루틴 상세정보 생성
    Former-commit-id: c65f00463f989c94af86e9c3dd0a2fcf6b1d8156

--- front-end/src/components/Fitness/FitnessList/FitnessPlayBottomNav.tsx
+++ front-end/src/components/Fitness/FitnessList/FitnessPlayBottomNav.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a591131ea3d0c4a1dcbb286e9f39d5001decb1770ce4392648a3fadb2317ae22
-size 1598
+oid sha256:1f5fe9dd1a812b256927bfed5076517a6e904283cd6f7bd46745f0a31b3fcbc9
+size 1544
---
+++ front-end/src/pages/Fitness/FitnessLoutineDetailPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3fb33a4eac65f00855fdb169456d9c2ca6169db7e00f5e03593dc18d30cb90a1
+size 1150
