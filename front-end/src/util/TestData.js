98a237fb7a787902e5645f153ecff60df6b4d40c
JinY <wlsdud6221@naver.com>
2024-08-02 02:53:28 +0900

    test - ojy - 테스트 코드 추가
    Former-commit-id: da13f36f8b0becd96f53c3932f6ccc900f171c70

--- front-end/src/util/TestData.js
+++ front-end/src/util/TestData.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2af8d12ae8d0bc2afe20a2c42cb7e216ea8ee37908c927812f0d270f4754e4f4
-size 1948
+oid sha256:28470a4b41e402954af8a38076003ae8fe67feea3d1d3e7186154c79b4668c63
+size 1953
