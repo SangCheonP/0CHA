2e419326b2cf0a8a4e6c401d9e419b0157d7cb72
qqa108 <qqa109@naver.com>
2024-08-01 17:48:39 +0900

    feat-cyh-sns-feed구현중
    Former-commit-id: 3883bbe70d518ec4a40b61a9679681c037700f7c

--- back-end/src/main/java/com/ssafy/back_end/sns/mapper/SnsFeedMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/mapper/SnsFeedMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f0aad16485586a08aea84f9459852899d2e63bf491ccd5c142d40fe189eded2
-size 75
+oid sha256:9765a38ccca59a7aa91df69b4ea68c8d6c1999ebf45250bc44193a3529cb8c08
+size 130
