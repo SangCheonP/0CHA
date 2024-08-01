4b63003b655a0d9acf7fda4e63d22f92f09f8f26
qqa108 <qqa109@naver.com>
2024-08-01 17:24:29 +0900

    feat-cyh-jwt완료, sns-feed진행중
    Former-commit-id: e80c4812f8f1e43a9a541bda45b136076c8623ea

--- back-end/src/main/java/com/ssafy/back_end/auth/mapper/UserLoginMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/auth/mapper/UserLoginMapper.java
@@ -1,15 +1,3 @@
-package com.ssafy.back_end.auth.mapper;
-
-import com.ssafy.back_end.auth.model.UserDto;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserLoginMapper {
-    UserDto login(UserDto userDto);   //로그인
-
-    String loginRefresh(UserDto userDto);   //액세스 토큰 재발급
-
-    String social(UserDto userDto);   //소셜 로그인
-
-    String socalToken(UserDto userDto);   //소셜 로그인 회원인증
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:227c535f9189a1863ded04071f1cad2a34ae280eb2a6714e056446c029f0e87a
+size 586
