c76c14cc53021ec5958b2dcec4d8dcebbca4c9c5
JinY <wlsdud6221@naver.com>
2024-08-02 02:53:45 +0900

    init - 인바디, 운동 기록 차트 페이지 생성
    Former-commit-id: 3f7df078035f5fb5460a42922ab69ac7240fe526

---
+++ front-end/src/pages/Record/RecordFitnessChartPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b94c155c3867f10490b5aa5cb013528e132dcf6f465b164526be9891554365e1
+size 1667
---
+++ front-end/src/pages/Record/RecordInBodyChartPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cccdeeac860ac306a8654df2349ac5fdecfd0065be19b622d1f4b66eaee00977
+size 2741
