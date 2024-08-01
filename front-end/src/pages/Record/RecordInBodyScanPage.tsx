7f7c8dbef83d41a9436732c3355ec94fcdf4a64e
JinY <wlsdud6221@naver.com>
2024-08-01 17:43:02 +0900

    init - ojy - Record관련 페이지 및 컴포넌트 생성
    Former-commit-id: 18d05eb2c7c7145580ecb11b050799d5b8762d54

---
+++ front-end/src/pages/Record/RecordInBodyScanPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ddb5a204eb5773f75a4ff35ac63653ffb0197bac868e2f698e97fa4b464bc5e0
+size 1695
---
+++ front-end/src/pages/Record/RecordInBodyScanResultPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0ca9682a29c0c59c8fdce6e17578524c993ca22ceb69a93ec67a7d1614b402a
+size 4112
