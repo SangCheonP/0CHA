48e5444b95bfc897beee9a393ec608712e871c4e
Park SangCheon <pswlove38@naver.com>
2024-08-02 12:58:40 +0900

    Add WebSocket, Add CORS
    Former-commit-id: 44c815958dce40aa35027cc322941b61aeeb40c3

--- back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsChatController.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ff2e57a3620fb32327282e551364c7b1a286fe8e3cefd8e286d2c6d0aa2c971
-size 1424
+oid sha256:683af8ff7d3a6df4e39e1614c74f318a90037408c49cf05b9c1af927d7b81aa3
+size 1319
