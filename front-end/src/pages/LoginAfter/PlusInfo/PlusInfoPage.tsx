0363987c2b8d384601e589954d7e81041daa4606
복현우 <bhwoo1001@naver.com>
2024-08-02 17:46:52 +0900

    feat-bhw 비밀번호 찾기 로직 구현완료
    Former-commit-id: 80bf0ff9383d415fadc7be8c201cb9b144b7e77c

--- front-end/src/pages/LoginAfter/PlusInfo/PlusInfoPage.tsx
+++ front-end/src/pages/LoginAfter/PlusInfo/PlusInfoPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7ab4dbb48aeb8dfb7296f085c835e1b07ce56dffcefcc52bd83bb53dd29afcc
-size 11858
+oid sha256:9125657b16ff1de6dc8cda10d22bae6ebf768ba537fe0b788ad0248f1b6ee7e7
+size 12020
