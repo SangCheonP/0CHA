20aa39a3d47dc359972eb753873d8c81d1c5aa55
qqa108 <qqa109@naver.com>
2024-08-02 17:08:50 +0900

    feat-cyh-sns-item구현중
    Former-commit-id: 952ac5dde717af72aa205141efbf7b8c8e1faffe

--- back-end/src/main/java/com/ssafy/back_end/sns/mapper/SnsItemMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/mapper/SnsItemMapper.java
@@ -1,4 +1,3 @@
-package com.ssafy.back_end.sns.mapper;
-
-public interface SnsItemMapper {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ed5d4f319abcc8ba87f270fd2159e3deecf6318e8f895180526035261d262b9
+size 974
