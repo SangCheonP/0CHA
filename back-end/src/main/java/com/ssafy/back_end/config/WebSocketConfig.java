dfab22566ff815ad68f8bb7c9fd21a87860f7a4c
qqa108 <qqa109@naver.com>
2024-08-02 14:00:41 +0900

    feat-cyh
    Former-commit-id: f31fe2380398cfd3906c814b804dfd2ff34312a5

--- back-end/src/main/java/com/ssafy/back_end/config/WebSocketConfig.java
+++ back-end/src/main/java/com/ssafy/back_end/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2bf92d2f684366279abd6bc8270f75002bfd893f6405cad8ae5f14e20eea93b
-size 1232
+oid sha256:099e3d8499a1cce46240a392a2a35b8546e55367f72b124c9c2a83b66070a45f
+size 1203
