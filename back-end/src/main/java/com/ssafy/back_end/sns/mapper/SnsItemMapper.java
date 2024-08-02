ee64204cbef4d8083b4e8af0be3f78d1a0a5de6f
qqa108 <qqa109@naver.com>
2024-08-02 17:11:55 +0900

    test
    Former-commit-id: 49ae16776c7efc30f9c3669cfd45edc39edc7a19

--- back-end/src/main/java/com/ssafy/back_end/sns/mapper/SnsItemMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/mapper/SnsItemMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ed5d4f319abcc8ba87f270fd2159e3deecf6318e8f895180526035261d262b9
-size 974
+oid sha256:e2954db3195cf7b5c5f92ed3337d07a010616131ee59ecb57709b878595e219b
+size 975
