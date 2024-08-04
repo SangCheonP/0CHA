5aeeb896060f8be719c78167852f711a8ab27476
JinY <wlsdud6221@naver.com>
2024-08-04 13:35:29 +0900

    feat - ojy - redux-toolkit 적용
    Former-commit-id: 7aabed377f008160378f89db8dc86a2ca68ff86f

--- front-end/src/components/Common/BottomNav.tsx
+++ front-end/src/components/Common/BottomNav.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f49c060e9c0ee6946ae5a0bc31468d8c3a36fff689e5ee75fb40591706c42fc
-size 2199
+oid sha256:9fa9a2d14ba598aa6efcb1b3b245521322edd781c9489092bad05ad2821f3d39
+size 2408
---
+++ front-end/src/store/index.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:edfe58dc47132cd47d2ef2ed88de9570371c959e9ecc24095b100f5c2656f24c
+size 836
