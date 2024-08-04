4ff5b752d66dc58546a212bbb59dc04621757261
JinY <wlsdud6221@naver.com>
2024-08-05 03:36:20 +0900

    fix - ojy - modal 제어 변수 재설정
    Former-commit-id: f315fcc24eb04a407f0f44a41dc4aa407f554a85

--- front-end/src/components/SNS/UserSearchModal.tsx
+++ front-end/src/components/SNS/UserSearchModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afb3ae6a51ce81d93f850b44727ef594f74c3507e1d3a64cb2420a7672a00a22
-size 1544
+oid sha256:b3a4e766a1093bc21b43512f22d455e44a0dd83646c9b411c15253eefad818b3
+size 1529
