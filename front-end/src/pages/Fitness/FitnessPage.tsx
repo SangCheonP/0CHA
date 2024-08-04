cf540a0db6d685ce16b5e97370f306282d6bf9a9
JinY <wlsdud6221@naver.com>
2024-08-05 03:31:30 +0900

    feat - ojy - fitness 라우팅 설정
    Former-commit-id: 70179927888fcf51ae9da168859995f19f888f29

--- front-end/src/pages/Fitness/FitnessPage.tsx
+++ front-end/src/pages/Fitness/FitnessPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af44f0062367109ea544cdb1bf48ad4d53ba8ff40bc3205fc13c84bd2c435251
-size 1543
+oid sha256:4d539b9d5c5eaf48454dff5df3d7a0bdd37a8e5334450541ace76ad0feffa782
+size 1792
