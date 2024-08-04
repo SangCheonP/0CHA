844b1dc82cca2daaddf465889cd051cc84efef61
JinY <wlsdud6221@naver.com>
2024-08-05 03:32:46 +0900

    feat - ojy - 전체라우팅 설정
    Former-commit-id: 97074c13c788d4e0c045f1117faa721d9a55b87c

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c76e24f8ccddf9632dd58e0579991f8005116e48328539a85154d466fabb97b
-size 7240
+oid sha256:7c90087ebc1bf15fb944bd9deee839de5029c22f65a737b87cf4e0d43a6091fd
+size 9682
