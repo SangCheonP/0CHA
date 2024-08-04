a54d762bd3ad691f088d7aadf1f5c0bde69b9071
복현우 <bhwoo1001@naver.com>
2024-08-04 12:56:37 +0900

    feat-bhw 회원 관련 페이지 1차 로직 완료
    Former-commit-id: 15b0cfad6b7ed4333030748979fff1a4280276ee

--- front-end/src/pages/AI/AIMainPage.tsx
+++ front-end/src/pages/AI/AIMainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c01562d3c2c37f6e33224aef4eb7c5e0367ed5a9742327e4b66632eef28623
-size 5274
+oid sha256:eb2196be308c5b5c22f727861f165a445ef9b35b603f136de603fb679f822965
+size 5252
