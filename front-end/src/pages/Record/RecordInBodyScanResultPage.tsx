42d2fe48fdea8c1d43a2eddbc95ccd830ae55310
JinY <wlsdud6221@naver.com>
2024-08-05 03:31:49 +0900

    feat - ojy - record 라우팅 설정
    Former-commit-id: 87f818c98cddcaea8c971317c92b4cb06ced7f4d

--- front-end/src/pages/Record/RecordInBodyScanResultPage.tsx
+++ front-end/src/pages/Record/RecordInBodyScanResultPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ca9682a29c0c59c8fdce6e17578524c993ca22ceb69a93ec67a7d1614b402a
-size 4112
+oid sha256:5f617d7e28e1c4f73e47085b741e5e4549495df82448400eb80f29dcecf64ae3
+size 4120
