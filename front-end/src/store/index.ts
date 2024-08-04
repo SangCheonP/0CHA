21c8f08662ee0f1913ece6de73cc778ac966a9bd
JinY <wlsdud6221@naver.com>
2024-08-05 03:28:16 +0900

    feat - ojy - 상태관리 영역 추가
    Former-commit-id: f54dc005c89908626392a76590e6f035ea3e6d61

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:980cf2ea5abe0bf210d30691a9198376cac113b5a8a3a983616d2c339902f24e
-size 808
+oid sha256:2e4fb437d1f13cbbd6fa5bd95317715d4198598829d4120ec6e8d7aa189b2c1b
+size 930
