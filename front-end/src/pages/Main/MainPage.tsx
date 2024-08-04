d459de2ada11d5abe9c621734d4b902baff19cac
JinY <wlsdud6221@naver.com>
2024-08-05 03:32:28 +0900

    feat - ojy - 메인 및 사용자설정 라우팅 설정
    Former-commit-id: 920245f52660113ebe55d108002fb773ad3bce2a

--- front-end/src/pages/Main/MainPage.tsx
+++ front-end/src/pages/Main/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c9670a463edc52eb5cc515cea28fac359ba5ba2ef18c2d73c2eb5c69ae12341
-size 7480
+oid sha256:36e3a7cfb50b03ef342792a90c704ca229e5d16c5f843104edc4a97a2ce187d5
+size 8132
