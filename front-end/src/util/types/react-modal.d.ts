96b5b69dd5123f3cd8ec4a3d174a7502aa4ac484
JinY <wlsdud6221@naver.com>
2024-08-05 03:29:33 +0900

    style - modal css 수정
    Former-commit-id: 0bde4c00697aa4c4318a33d9cf278c51b1aeb116

--- front-end/src/util/types/react-modal.d.ts
+++ front-end/src/util/types/react-modal.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08a49d409b125431ceb2b7999762ce610a660680541e0b703a2d3ccaea65cf73
-size 564
+oid sha256:d33005c361045fff0ac8acb85bccd779a6631350a1bac62db8cc3d864e00a9fa
+size 591
