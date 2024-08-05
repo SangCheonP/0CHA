e0cac0d7e262baff3cd0e11f004bc9f7cbc368c5
JinY <wlsdud6221@naver.com>
2024-08-05 16:00:29 +0900

    fix - ojy - strick 해제
    Former-commit-id: 2c8cf95f4df1360f9a67bad40fa32259a9420ead

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80426996bdf68cdd8d0aba59136d243d2455dd0028aaa80dc366ed7cf48ca858
-size 1300
+oid sha256:668e8dfc1ebf7c8c61a8e095ba1f3df9d558287f3a742ba201b97891edd1c2f6
+size 1245
