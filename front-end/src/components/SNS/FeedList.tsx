54dadbb30a757d2bdc1000673b14ef9b86def40e
김고은 <kke0402@naver.com>
2024-08-05 16:15:11 +0900

    add FeedTestData
    Former-commit-id: de68888256a30a0f1df47f62237648df7912273d

---
+++ front-end/src/components/SNS/FeedList.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:828db730f4994d59bd9155cc90f22e4395c627d82d91866a169c156090474aee
+size 3829
---
+++ front-end/src/util/FeedTestData.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df74ce783621eb8936b5268f955d288624378d220f3b1766ce24311bbf118fc1
+size 11145
