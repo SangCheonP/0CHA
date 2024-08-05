715693cfd9e26fe87f78696f5bca2f2585cffcf9
Choi yonghun <qqa109@naver.com>
2024-08-05 18:34:18 +0900

    feat-cyh-sns,exercise,routine-merge
    Former-commit-id: 0661d866f5d51b5dfefdfe08241841043c92ffd3

--- back-end/src/main/java/com/ssafy/back_end/exercise/model/RoutineDto.java
+++ back-end/src/main/java/com/ssafy/back_end/exercise/model/RoutineDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a29e43467a575bd95ed1fa80d71fa477efbd341ebcb55f295122cea6db1cc54
-size 544
+oid sha256:2fb2b47d2ce3fcb5b702ff2e8e18edfda3a39f36b6d2e890caf2bf2c9ba097f7
+size 519
