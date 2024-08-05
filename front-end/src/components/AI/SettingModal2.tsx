c42ca2a9f8bc09522329eba4ed1e12cbe0df43e4
복현우 <bhwoo1001@naver.com>
2024-08-05 17:48:19 +0900

    feat-bhw AI 모델 화면 연동 구현 중
    Former-commit-id: 700f405785968af3ac7d9c67324e1e87d44a7752

---
+++ front-end/src/components/AI/SettingModal2.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86a4b7ee6c0137d445e01780bdec9451e965f585accd02f3acb4a08a1b863f36
+size 7900
---
+++ front-end/src/pages/AI/AIMain2.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a36d2086fb3a6bdec72c7cc979810cc05f0cc4de696cd7ac2e631259ab97f8b3
+size 9447
