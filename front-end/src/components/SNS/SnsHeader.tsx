d67cebe9554d3242e62d12317e2158698b4f9b80
JinY <wlsdud6221@naver.com>
2024-08-05 10:09:04 +0900

    style - ojy - css 수정
    Former-commit-id: 953a0586126e78b84dfef901ae3ab1b2c18b3f04

--- front-end/src/components/SNS/SnsHeader.tsx
+++ front-end/src/components/SNS/SnsHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a5c61fb3c6dc910d67b5a03a8059c95962fe7858cfc07761444e7821410f2a6
-size 1781
+oid sha256:2b91fe771bce5a0d54dd0e6c44a390b38b67f6ad3ed3be224d8945e3961a02c3
+size 2277
--- front-end/src/pages/Main/MainPage.tsx
+++ front-end/src/pages/Main/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36e3a7cfb50b03ef342792a90c704ca229e5d16c5f843104edc4a97a2ce187d5
-size 8132
+oid sha256:500306fae528b1093be0e32d28ef60368978a84ab56115da26477254272c0d32
+size 8154
