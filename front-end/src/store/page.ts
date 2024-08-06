760c694f5e9844a652e9b2c958a19fa4a73aee49
JinY <wlsdud6221@naver.com>
2024-08-06 17:54:22 +0900

    feat - ojy - 페이지 제어 변수 설정
    Former-commit-id: 8c0aa0b94b0f3640d33abe6e9077323d23b7e85c

--- front-end/src/store/page.ts
+++ front-end/src/store/page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5372e7e70af92849b684769ab4abaf1998a61b6f83ed7f0813f4f53a0c766a9f
-size 590
+oid sha256:15512e64e022eb4ee9fd58974d0d99cc7493049921701ee45a07870da639ced2
+size 1892
