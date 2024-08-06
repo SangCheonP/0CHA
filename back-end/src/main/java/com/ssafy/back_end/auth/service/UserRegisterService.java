e0a8c94c7de339e5006f7d55578e8800c722b9c8
qqa108 <qqa109@naver.com>
2024-08-06 17:34:51 +0900

    feat-cyh-email인증완료
    Former-commit-id: b74fd9ca4cba691cbaae0a4f697be53adc969337

--- back-end/src/main/java/com/ssafy/back_end/auth/service/UserRegisterService.java
+++ back-end/src/main/java/com/ssafy/back_end/auth/service/UserRegisterService.java
@@ -5,7 +5,12 @@ import com.ssafy.back_end.auth.model.UserDto;
 public interface UserRegisterService {
     int register(UserDto userDto);   //회원가입
 
+    int userInfo(UserDto userDto);   //추가정보 입력
+
     int checkEmail(String email);   //이메일 인증번호 발송
 
     int checkNickname(String nickname);   //닉네임 중복체크
+
+    void sendEmail(String email, int authCode);   //이메일 인증 전송
+
 }
--- back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsItemController.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsItemController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:850cd2fc481ba99f89895dc05be50cc05ce677dbaa3de89fcbbe41b4570a6660
+oid sha256:1f9455adff2410a78a77d048a7251d3dcd66922c83d4330c3ba8f2667d43cd68
 size 5481
