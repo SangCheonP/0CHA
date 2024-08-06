1b4a78aac8f9506236d7c2058f900c2cd591cd5a
JinY <wlsdud6221@naver.com>
2024-08-07 04:23:44 +0900

    fix - ojy - jwt 없는 axios 세팅
    Former-commit-id: d0ddeb3a753bec84c55fbb9bf81b29d9383e4954

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2891254212d06d3ecc764fe4cf792e743a11d448f1a901882db218e8335fccec
-size 2253
+oid sha256:002f5f595dcf8531bc3912718cd6f319ad8ddef3f7020d6f89c0370bdcc69ff8
+size 2625
