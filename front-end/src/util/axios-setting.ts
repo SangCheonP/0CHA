e90781235c841c851883e39eed2f16e4e869f472
JinY <wlsdud6221@naver.com>
2024-08-06 17:54:49 +0900

    fix - ojy - axios 타입 및 추가 설정
    Former-commit-id: b343056b0efd7d5a7b6d55440a4437f4318c0253

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,13 +1,3 @@
-import axios, { Axios, AxiosRequestConfig } from 'axios';
-
-export const localAxios = () => {
-  const instance: Axios = axios.create({
-    baseURL: '/proxy',
-    withCredentials: true,
-  });
-
-  instance.defaults.headers.common['Authorization'] = '';
-  instance.defaults.headers.post['Content-Type'] = 'application/json';
-  instance.defaults.headers.patch['Content-Type'] = 'application/json';
-  return instance;
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:2891254212d06d3ecc764fe4cf792e743a11d448f1a901882db218e8335fccec
+size 2253
---
+++ front-end/src/util/types/axios-user.d.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa386f0a32d588a940d0c4f69397d0c8b7d0758e88c4ea94a1c4bd4ec615ae79
+size 566
