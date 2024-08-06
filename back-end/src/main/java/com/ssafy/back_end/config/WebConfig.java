052afd8a99c821180addc35db1eb55681afb77ed
qqa108 <qqa109@naver.com>
2024-08-06 09:01:56 +0900

    test
    Former-commit-id: 7a9057f84bebf1e7bada2aa0fd883a85c73eb4f4

--- back-end/src/main/java/com/ssafy/back_end/config/WebConfig.java
+++ back-end/src/main/java/com/ssafy/back_end/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b87ce75622ef5eefe6279a339f62b818e747d7b6e5a1a3f4d81cf304f5132fe
-size 1562
+oid sha256:1f8fabef871eacf3bd9c8f5924fe76b641fd02736f1c179408b321a5645d1f07
+size 1163
