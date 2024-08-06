842fd471a462c9d9743a2a8cd0afc94039aa6624
qqa108 <qqa109@naver.com>
2024-08-06 13:37:50 +0900

    feat-cyh-sns-isLike추가
    Former-commit-id: 6200b51a016d5484559770c2bc23ddb8669cd8d6

--- back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemService.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c90809298aecf054ef25c551751532519f4b84c16470fa26ebb5fbb1d6868c9a
-size 839
+oid sha256:e7b51205702467ab6c96bd5615b611e11f64e68caaf787821d995e7a37afb6f8
+size 858
