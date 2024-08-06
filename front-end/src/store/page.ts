fcb65c512ba1b16048b3eeef40b83410ac2700f0
JinY <wlsdud6221@naver.com>
2024-08-07 04:24:01 +0900

    fix - ojy - 이메일 제어 버그 수정
    Former-commit-id: 5d9bb6fbc996ad9b9e98c2499febeaffebcc1fa4

--- front-end/src/store/page.ts
+++ front-end/src/store/page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15512e64e022eb4ee9fd58974d0d99cc7493049921701ee45a07870da639ced2
-size 1892
+oid sha256:d39fc1b9ac187e19269de90a109df5ecdf4b1954c73e1456e450f1513a6fbe5f
+size 1893
