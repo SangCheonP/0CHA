7e18e127cf6ce3975e132e85f4b81b668aea3566
nue-ok <kke0402@gmail.com>
2024-08-07 22:46:29 +0900

    feat-kke-피드 좋아요
    Former-commit-id: 417e3c66c9163c0c379c58129272c70ad0221bf9

--- front-end/src/components/SNS/FeedList.tsx
+++ front-end/src/components/SNS/FeedList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b1d9a433dac6c2a8a2087f5130097ebdfbe325037ff103bdf009c5b1fafe2f3
-size 3834
+oid sha256:1c849a108ce4649628505c4419ecb4c984bdfc757306bcb40930fd90c17226b0
+size 3245
