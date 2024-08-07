520228d85dc2ffb8a05a04ccc9d99e192708abce
qqa108 <qqa109@naver.com>
2024-08-07 13:54:46 +0900

    feat-cyh-record-inbody완료
    Former-commit-id: 16b2c0bcb2aa171d016cd9dd352948982e25b50f

--- back-end/src/main/java/com/ssafy/back_end/sns/model/FeedInteractionDto.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/model/FeedInteractionDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f385a8c97a251e69080f1f7ddc6e4e45628d71d4af3707ed6045f9ecf1ac4bb2
-size 412
+oid sha256:718125cf4ccffe020b68652e3ae9ecf5716812f0da5c10062554d4a9391a25bd
+size 411
