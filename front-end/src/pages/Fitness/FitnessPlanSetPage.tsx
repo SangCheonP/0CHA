e5bb9a92670a12569cc69f54f2ecf6d50c16c018
JinY <wlsdud6221@naver.com>
2024-08-08 04:33:10 +0900

    feat - ojy - 운동관련 axios 적용중
    Former-commit-id: 970b51a41790f97d9544e03b39c808b0f84c11f6

--- front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
+++ front-end/src/pages/Fitness/FitnessPlanSetPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:920a2b7dde8dfd84a36b426576b17ad86a4d07767fb440abf2847f0ea014efb8
-size 4029
+oid sha256:10e14fa0f59bc538306fbfaea23a916abcffe63a98e7a4280bd9496ef3255fb4
+size 4361
