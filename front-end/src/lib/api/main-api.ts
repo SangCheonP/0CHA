2b272a2e48057e83e8e40f41decf65bcc01724a8
JinY <wlsdud6221@naver.com>
2024-08-07 15:15:45 +0900

    fix - ojy - endpoint 수정
    Former-commit-id: ae7eb87819b82d2ed951f5be23d6d0b644d79166

--- front-end/src/lib/api/main-api.ts
+++ front-end/src/lib/api/main-api.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd1d9d1ce733957a6a5f0719ab7ebdf58dc54997c8b5507185d393f12eb68d01
-size 2114
+oid sha256:2396164ab0fcdea5ce77b6aa1a23c5d1f8801a0b196ad6fba8ff0d726d2433f8
+size 2092
