12652132600f2ec8afb79ec6e5ffbee53e9194b8
ANSMOON <munbeumsu@gmail.com>
2024-08-07 15:09:22 +0900

    feat-moon-git conflict 해결
    Former-commit-id: 2fa6555a8370bedfbaa11791b621aaab9e86628d

--- back-end/src/main/java/com/ssafy/back_end/config/WebConfig.java
+++ back-end/src/main/java/com/ssafy/back_end/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e7653cc27b5f3571a4ee6e65af7f6acf3d35c15822e965818209b7c14e2f856
-size 1766
+oid sha256:92092d977c2756b6255d2d7904cad50f3e94a514d2a2df6e72a0e8c42f5f15fc
+size 1744
