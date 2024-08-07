6ff27a12d0fd0026832b2b9e42fd40e47361c0f8
Choi yonghun <qqa109@naver.com>
2024-08-08 02:04:52 +0900

    feat-cyh-jwt강제로그아웃구현
    Former-commit-id: e495d7b125a53d1b6197f1f3b6aae818a8383b95

--- back-end/src/main/java/com/ssafy/back_end/auth/service/UserLoginService.java
+++ back-end/src/main/java/com/ssafy/back_end/auth/service/UserLoginService.java
@@ -1,15 +1,3 @@
-package com.ssafy.back_end.auth.service;
-
-import com.ssafy.back_end.auth.model.UserDto;
-
-public interface UserLoginService {
-    UserDto login(UserDto userDto);   //로그인
-
-    void storeRefreshToken(int userId, String token);   //리프레시 토큰 테이블에 저장
-
-    void invalidateRefreshToken(int userId);   //리프레시 토큰 삭제
-
-//    String social(UserDto userDto);   //소셜 로그인
-//
-//    String socalToken(UserDto userDto);   //소셜 로그인 회원인증
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff98c4a158a2c0b445862b9216eb30a48a33d90585cdb37aeb3d0efcb9b70943
+size 591
