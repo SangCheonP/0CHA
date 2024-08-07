9d3d911c28592df7dbfa08d76c60c3c0bf717e4d
김고은 <kke0402@naver.com>
2024-08-07 17:44:54 +0900

    feat-kke-피드페이지 api연결중
    Former-commit-id: 9da753bd80fd2a88fd75af06109f3d962d772ade

--- front-end/src/lib/api/record-api.ts
+++ front-end/src/lib/api/record-api.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37b62db95e33e073ee204e83803713c38df6bc77e7c77a2471436ab63b74cc61
-size 1572
+oid sha256:ee4213d239a924b12c47805cb39efa5d364f82f19486ce05c0ee55fb6d7937f5
+size 1528
