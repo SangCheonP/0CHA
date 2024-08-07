9d0a4d0ffc17d31661b0eff60cc98f3def8f162c
JinY <wlsdud6221@naver.com>
2024-08-07 10:34:23 +0900

    fix - ojy - 서비스별 axios type 정의
    Former-commit-id: 6cb6e60e4ab5b3a0e0015b742b8dbb5851fee552

---
+++ front-end/src/util/types/axios-main.d.ts
@@ -0,0 +1,16 @@
+export interface Profile {
+  nickname: string;
+  profileImage: stirng;
+}
+
+export interface MyInfo {
+  height: number;
+  weight: number;
+  distrct: string;
+  siGunGu: string;
+}
+
+export interface changePw {
+  curPassword: string;
+  newPassword: string;
+}
