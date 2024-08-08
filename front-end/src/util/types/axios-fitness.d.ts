c20bee509a1d879ed0165ffd9f7773f4f701f523
JinY <wlsdud6221@naver.com>
2024-08-09 05:12:53 +0900

    init - ojy - 리스폰 타입 추가
    Former-commit-id: 5614113b22628ac39118bdc3a59c0884df8ed35e

--- front-end/src/util/types/axios-fitness.d.ts
+++ front-end/src/util/types/axios-fitness.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f757b6649d922ab2e50f6ddb35764ea0ecbe7dfe5476d539bcec2a2074400b10
-size 1453
+oid sha256:bca110a2c512017aecc93ba01640aae9f89f8ddd5f20ae3b20afb661d849f434
+size 1968
