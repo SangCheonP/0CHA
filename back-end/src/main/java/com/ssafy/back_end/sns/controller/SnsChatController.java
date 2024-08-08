99e4eb28aec60621544de46f7bed7d3f293e064d
Park SangCheon <pswlove38@naver.com>
2024-08-08 14:10:59 +0900

    feat-psc-cors
    Former-commit-id: ace3dc1a9871563535b1b1a07cd69f7008b549d8

--- back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsChatController.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ea911377d3379f589d63df6c777c8c9e445e2df31438a8c481c0f845234cede
-size 5378
+oid sha256:2e334af16045cd769cc65e5db491089aae8c616082924fae1605f6203d257d15
+size 5396
