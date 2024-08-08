a538861fcd728d0283adcc3eaa0062ccf1cb77e1
JinY <wlsdud6221@naver.com>
2024-08-09 05:13:42 +0900

    feat - ojy - main, fitness api 연결
    Former-commit-id: 593848a997ca6d2dc9311dbc2181e45c30eaf896

--- front-end/src/components/Fitness/Detail/FitnessPlan.tsx
+++ front-end/src/components/Fitness/Detail/FitnessPlan.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6a8d6d6cf9bc0450abb9f6e379a0fd3ae49d425cc57c748b506a5ec8b9a0429
-size 5772
+oid sha256:4c5452832523891c849dcefa5b1c429712b506585311ae3bab873943261efa33
+size 5896
