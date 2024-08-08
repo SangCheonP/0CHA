ac18aa9a6ecdfda9b3700c59e31fb73249d896ed
JinY <wlsdud6221@naver.com>
2024-08-08 17:50:47 +0900

    feat - ojy - 선택한 날짜 ux
    Former-commit-id: 521c438b88c3a81669ce9752d0e0176c8d0d42a0

--- front-end/src/components/Common/CalendarArea.tsx
+++ front-end/src/components/Common/CalendarArea.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9398478026edfa21b07165223c25119eb69f27de0914e2a93e7a2ff763b31a0c
-size 1742
+oid sha256:3d4116b30467a9905b890d82260cc0c86d89ba685328a7f4b273eb06ae22fcaa
+size 1858
