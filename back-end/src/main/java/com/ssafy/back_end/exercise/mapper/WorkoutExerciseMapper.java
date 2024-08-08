ab776a1614b669574984551bdd1fa584c4258ab6
ANSMOON <munbeumsu@gmail.com>
2024-08-08 13:45:15 +0900

    feat-moon-운동 조회 좋아요 여부 표시 추가
    Former-commit-id: 82d83a4e5b434438c27f1bf2572ac8872d5eaa2f

--- back-end/src/main/java/com/ssafy/back_end/exercise/mapper/WorkoutExerciseMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/exercise/mapper/WorkoutExerciseMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39593238e33715b7e10500831276dceb1adc74e0cf708b0c187d257ed27235aa
-size 887
+oid sha256:336b8a7dee529ed5f24f94e16750d8f84b6b70d6c3116a8ecda62f399c30e651
+size 1158
---
+++ back-end/src/main/java/com/ssafy/back_end/exercise/model/ExerciseRecordDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca902a4a6191af0f30362c4b1eff86dfc5a33099db67b5352cd4059a15582e09
+size 346
