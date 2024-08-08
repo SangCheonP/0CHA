1d60475f1cc267fbb8263eda2a9bb82ee6639024
JinY <wlsdud6221@naver.com>
2024-08-08 13:10:27 +0900

    test - ojy - 배포환경 테스트
    Former-commit-id: c420090997ba4a5f6d2ab2d13ebfac4b08fe4361

--- front-end/src/lib/api/fitness-api.ts
+++ front-end/src/lib/api/fitness-api.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b38901908c8e520fd0c00658956e1c2f757b4ad3e15c89ff049e9c5fa68ccf56
-size 3858
+oid sha256:fa3a37353ed3aeccd9ea39b501b101b4ea31a5114d13b43fc76f7667f261e9d2
+size 3822
