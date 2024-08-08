d03b45156c5fd86f11c2427ed3f9a1b3ec51bc72
JinY <wlsdud6221@naver.com>
2024-08-08 16:23:53 +0900

    fix - ojy - error 수정
    Former-commit-id: e0c7507ce1b2e375061c16c47ead5a3c8b06a785

--- front-end/src/components/Modal/FitnessPlanSetModal.tsx
+++ front-end/src/components/Modal/FitnessPlanSetModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1424dc1544c32b424482a4fbfc3d2ce20e3be04a8823daaf013d7a5863433157
-size 3712
+oid sha256:bf60ab312b013379cc64390367c7d8d77c787fc969c3eeedd461f4ff925bb6e8
+size 3715
