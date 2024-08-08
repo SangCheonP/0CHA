0f06a09c8ee05ae10f36e49757b5ad90c1788871
최용훈 <qqa109@naver.com>
2024-08-08 10:08:31 +0900

    Update back-end/src/main/java/com/ssafy/back_end/util/JwtInterceptor.java
    Former-commit-id: 458c175bbcfce6a97395ac6dc6f26cb0e6e63510

--- back-end/src/main/java/com/ssafy/back_end/util/JwtInterceptor.java
+++ back-end/src/main/java/com/ssafy/back_end/util/JwtInterceptor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09dfaf1cb5da7010ad26f6b1866f7157575a1d2669257ffba7ff511d1021bd4c
-size 3560
+oid sha256:d4eb5432b2629d6b5411773df7715603ef8dfda6971c5d207b8f626b16a70fb9
+size 3589
