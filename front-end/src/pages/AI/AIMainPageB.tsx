741c1e8efc6d9bd20057d85ce083d5176982413d
복현우 <bhwoo1001@naver.com>
2024-08-09 17:57:32 +0900

    feat-bhw 중고마켓 페이지
    Former-commit-id: fd304ef5eb8e8598904ac5d2634f6bcc132c24ad

--- front-end/src/pages/AI/AIMainPageB.tsx
+++ front-end/src/pages/AI/AIMainPageB.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ecb9735d2b15c0fc80191db987a3a3ba3f9460f6e07f863e1ff7c4cb9db2005
-size 7239
+oid sha256:f37dd876f246724a2447ab64dd665faec7f27685cb6d63c6c22a0973c81c6c72
+size 7454
