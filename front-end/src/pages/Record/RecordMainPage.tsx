211345294afa830f23b43ed56184f8752400e81d
JinY <wlsdud6221@naver.com>
2024-08-09 17:39:46 +0900

    feat - ojy - Record Axios 적용
    Former-commit-id: e65d20444b8171ced4dd09e34294e28346bab1e6

--- front-end/src/pages/Record/RecordMainPage.tsx
+++ front-end/src/pages/Record/RecordMainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b1a32be4442ca12caacf75830c2dd0c16c2864401b63fdfed16d2c6eda30a7e
-size 3689
+oid sha256:bda23ec51614bcde73b783af0bf6c3e97cab125e2447f23f6ec8f959b610b5bb
+size 5412
