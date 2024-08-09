2d930576bdfaf6e744ef71477518289090c39d98
ANSMOON <munbeumsu@gmail.com>
2024-08-09 10:00:34 +0900

    메인 페이지 운동 조회 기능 추가
    Former-commit-id: 85a09485ce391242d6ff8d22cd547666b52f5ab8

--- back-end/src/main/java/com/ssafy/back_end/exercise/service/WorkoutRoutineService.java
+++ back-end/src/main/java/com/ssafy/back_end/exercise/service/WorkoutRoutineService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c24ff83d948acf35fad884e5648fd38d4cf83302097c7bb02b80163c664221e
-size 589
+oid sha256:14a728f20bf0ec8e3c6f476018e452586a9cd6b82220c1516ed7af062ebb3909
+size 644
