ded877425445c645223647409e829e2aee59dc6b
Park SangCheon <pswlove38@naver.com>
2024-08-10 04:04:40 +0900

    feat-psc-Upload image fin
    Former-commit-id: 3321c54a29ef516edd97d0c5ee32a309415e09f5

--- back-end/src/test/java/com/ssafy/back_end/sns/controller/SnsItemControllerTest.java
+++ back-end/src/test/java/com/ssafy/back_end/sns/controller/SnsItemControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb73b695ebc40609941c51ae774355e06df039488c2a527abbd2328428e597a5
-size 3895
+oid sha256:d0dad3c48b58b90965580c18ac9f06c9d9bdfcacbee63f2dc7059fffeb0c31ef
+size 3817
