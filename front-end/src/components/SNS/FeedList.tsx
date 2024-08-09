cec7f2f896225ce523c48390d31e94617ce57870
김고은 <kke0402@naver.com>
2024-08-09 17:17:58 +0900

    feat-kke-팔로우/유저검색
    Former-commit-id: 65b7f61c7cfc6d6713edb0a4d2f3c5fcdf6473e2

--- front-end/src/components/SNS/FeedList.tsx
+++ front-end/src/components/SNS/FeedList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81e0ce8ff9971ecac62f338b71ca718830b3626350e930c62c7d3c436c751c4f
-size 3417
+oid sha256:68cefec3c3a9533d71bfeedd10a6ccee2dbd55485a23b741c1874309ab59f777
+size 3421
