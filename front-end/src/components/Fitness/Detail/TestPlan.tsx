e82833d1c31271cdf71ec7064c5f05975f33d98a
Park SangCheon <pswlove38@naver.com>
2024-08-11 21:36:31 +0000

    feat-psc-modify file size
    Former-commit-id: 455cb0f657af121be8223a7ea063468aa41f722d

--- front-end/src/components/Fitness/Detail/TestPlan.tsx
+++ front-end/src/components/Fitness/Detail/TestPlan.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16cc9b44bd6e8b83539f4168ba1ee840b49c3e28afc749f75613a2a1f5a78beb
-size 6915
+oid sha256:d4b5fe1d5a0fdb8d8e94a461b889a75d0da6ebcbc66a663210ae27d44109a9aa
+size 6677
