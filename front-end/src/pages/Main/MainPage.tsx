bee2509f885c2e066b88f923354f99f77638d56e
JinY <wlsdud6221@naver.com>
2024-08-11 17:45:39 +0900

    fix - ojy - api 연결 및 UI 수정
    Former-commit-id: 8c5b5fb03f8e2cd03b3998d81cd18580c5ea5771

--- front-end/src/pages/Main/MainPage.tsx
+++ front-end/src/pages/Main/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f9ec4b22ad671e56019440aaa05053d0fb5520be0f3fb3a5b7604d97a22ad3f
-size 8711
+oid sha256:48d82890d1b04be136744d21fecbaf3633b2b2e65ede7dbc2b96aee8feb15dfe
+size 10962
--- front-end/src/pages/Record/RecordInBodyScanResultPage.tsx
+++ front-end/src/pages/Record/RecordInBodyScanResultPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8234e94bba0576ce223916238ea842170c738bacf36a33f0bfde194e0da8134b
-size 4643
+oid sha256:d00bb6da259beb7da99ada6d203431a00a7df6fae85b0176b0a1895b2c65b056
+size 6902
