5cf6d7d9677413ca896456f549ebbc74a368d3e7
JinY <wlsdud6221@naver.com>
2024-08-12 04:15:33 +0900

    fix - ojy - 깃 충돌 변경
    Former-commit-id: d6f4476ac32e24123b5ba09b525f57e5be7e8423

--- front-end/src/pages/Fitness/TestPlanSetPage.tsx
+++ front-end/src/pages/Fitness/TestPlanSetPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ca26bde4862488010144fb80988932aecc881bb101b9fe5bf58109a61cd2c85
-size 8600
+oid sha256:d38641a3648525c005dfe747d7707764d5bb887182bbc72af25a753b2a2d1e58
+size 8325
