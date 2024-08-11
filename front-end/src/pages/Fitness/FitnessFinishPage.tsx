0b0d82f0c45ab32288d38e2dc503b36403da4118
JinY <wlsdud6221@naver.com>
2024-08-11 20:30:27 +0900

    fix - ojy - axios 수정
    Former-commit-id: dee2cf8b55240fef6baa31df1b3818dd72268d30

--- front-end/src/pages/Fitness/FitnessFinishPage.tsx
+++ front-end/src/pages/Fitness/FitnessFinishPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc481af503d15f5aeeb6c96c230dd18eb6ecadaf85b0e8fa865d2c78bdd82eea
-size 4086
+oid sha256:e8993fb11d584bfb1ecab91dcfa9393b2249c1aeb28f9829df7211708b6b2b2c
+size 4084
