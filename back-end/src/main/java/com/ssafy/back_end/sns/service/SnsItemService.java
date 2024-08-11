c901b7eee151a5142e38eb2d0ecb3ae66f9e546b
Park SangCheon <pswlove38@naver.com>
2024-08-11 20:00:38 +0900

    feat-psc-Modify item write error
    Former-commit-id: 3d597b00df67e321e19cacc04dcea610b7b9b10d

--- back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemService.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7c3b4d37aad30dc38b605448ed43a188e3b767d6e6b5257738357d54f1f908e
-size 1124
+oid sha256:745fee34ef57dacb382951e693dba1635e1addfb0436b15c134312e4ed93010f
+size 1115
