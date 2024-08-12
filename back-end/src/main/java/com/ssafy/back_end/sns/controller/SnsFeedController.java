8d35630c1a31a12ec84caef50f1acfad089846d3
qqa108 <qqa109@naver.com>
2024-08-12 17:01:16 +0900

    feat-cyh-sns-feed작성, main프로필변경
    Former-commit-id: 94d767e17eed16e06e3ad84f99cff411fc049ffd

--- back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsFeedController.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsFeedController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5daf4736ef78e9fb1c57d7554669529f890c02d19a3dca4071a80ece0d6ad34
-size 13377
+oid sha256:836fc655f027402c602e578a05e67f41c5e31e7e2ac5557fd21c41dee055f0be
+size 13682
