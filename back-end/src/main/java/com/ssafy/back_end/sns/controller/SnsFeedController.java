fe49800f366df8c152ed339fbcb8ff5d65ae9bf6
qqa108 <qqa109@naver.com>
2024-08-12 09:08:53 +0900

    feat-cyh
    Former-commit-id: e83586c5ad7d9e374fbfefd0190275d3bb23d78d

--- back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsFeedController.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/controller/SnsFeedController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16368618989b7ac0e8b4328ba016fcca4cb13022f89cf1eba2769ca4e91cbe73
-size 10053
+oid sha256:cb1cd86211093d91c649de71a02d1830380f40455d97a5daabc3a12a92a9c4d9
+size 10041
