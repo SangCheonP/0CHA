af44a88fa4755f976fe9edd07864692274550276
Park SangCheon <pswlove38@naver.com>
2024-08-12 23:15:56 +0900

    feat-psc-Add CORS security
    Former-commit-id: 98ffe4c5627c7b0024b3c1a88a02a3e0b25f2677

--- back-end/src/main/java/com/ssafy/back_end/config/SecurityConfig.java
+++ back-end/src/main/java/com/ssafy/back_end/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:597a04769d2e8629f958dc2d98a30dccc3c4d094ef17d84466d5bfca305706ec
-size 4028
+oid sha256:861a20314dc73f2b626eccf6c1b245d2770f0f5559420fd3520d557a5477f9e2
+size 4712
