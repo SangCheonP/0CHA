71a93c654cdb37f7e60365d36f2a4e2e8a629faa
JinY <wlsdud6221@naver.com>
2024-08-12 17:56:58 +0900

    fix - ojy - fitness 수정
    Former-commit-id: 487a7c0dfb55194eb5b8a6672024d4e39ba650fa

--- front-end/src/pages/Fitness/FitnessListPage.tsx
+++ front-end/src/pages/Fitness/FitnessListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:253b9c6cd7ac551b1ee12ca5ea33d3eb8068359d2d6f083d8575800b561de615
-size 3922
+oid sha256:57aa06ad630b4eafa8976f80397619fd865bf477465ca0037212f5d7f735c46f
+size 10009
