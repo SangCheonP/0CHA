3abbe7d4b99f28188dbe811a82ffc261355693d9
JinY <wlsdud6221@naver.com>
2024-08-13 03:54:48 +0900

    fix - ojy - 운동 플레이에서 버그 수정중
    Former-commit-id: 3aca9b9044f2b2b9f4b5957ecbaa33d42038ed00

--- front-end/src/components/Fitness/Etc/FitnessPlayBottomNav.tsx
+++ front-end/src/components/Fitness/Etc/FitnessPlayBottomNav.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41ae6d47058686c80c25443a12af64abe93b337bac180c4c0d169b1cc6f6ba64
-size 4682
+oid sha256:e814e09a1b1629285578a1348d4cfb4dc2d6574984d2d4a86c448bbd61318436
+size 9853
