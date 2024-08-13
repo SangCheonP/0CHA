0b9474e0c6ca178f3349fa5a1b40bffc2f363939
JinY <wlsdud6221@naver.com>
2024-08-13 17:52:46 +0900

    fix - ojy - fitness 관련 수정 및 axios 수정
    Former-commit-id: ffdbb76dc0291ce3dbedf4936f46cf35cf7196b8

--- front-end/src/components/Fitness/List/FitnessList.tsx
+++ front-end/src/components/Fitness/List/FitnessList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf06b2dc8ae9164ed0b867e7d466b4b9872d1d15cac8286935ed7052c3db6f16
-size 4500
+oid sha256:83eb1e3169b651d4f43bcf37e4065fe926e319bd13dd9325cb62ece06b374fea
+size 4992
--- front-end/src/store/fitness.ts
+++ front-end/src/store/fitness.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23292436e030a60e2f9c1016e6bbf0e1342b78c62448e3630fc305e3c8953fb5
-size 3544
+oid sha256:0f67e00ec738d7ed8ad92a489408b1788d85b1e1cfad0ab27ad349fd3be619d7
+size 3692
