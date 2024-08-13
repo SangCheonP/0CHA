90275326c2eacddd5c1475580e1d3c706c894a96
Choi yonghun <qqa109@naver.com>
2024-08-13 22:30:19 +0900

    feat-cyh
    Former-commit-id: 75ad5a45144a184fd0d333bb46a0bee71c61937b

--- back-end/src/main/java/com/ssafy/back_end/main/controller/MainController.java
+++ back-end/src/main/java/com/ssafy/back_end/main/controller/MainController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48b158429c68f2a79f608f9c0d6bce5cfc2b2d5a51a11169e154a02513870d55
-size 11725
+oid sha256:354a056a686e083cbb9f973b1054f22afe85fa4d920dc419bc1c870e5424853d
+size 12037
