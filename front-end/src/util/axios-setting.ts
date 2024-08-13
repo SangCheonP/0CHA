b78964af674d0c5dfe546a82152a176021b54a8b
JinY <wlsdud6221@naver.com>
2024-08-14 08:44:12 +0900

    fix - ojy - 배포버전 axios 세팅
    Former-commit-id: cbb6a645e311ce06962f3cfba7db5854a2896d7d

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72b4a2a4b0b3bda8300d5b51401f2beaf1465f471df6560d5e7046c6f604b81d
-size 7026
+oid sha256:22ded133676d43f0203ff51c30b1a45500bb12c6162c82e7dcf160884892e0c7
+size 7070
