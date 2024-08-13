fae69fcbed38f3a20a892178462bbc787046ecdd
JinY <wlsdud6221@naver.com>
2024-08-14 08:39:20 +0900

    fix - ojy - 배포용 axios 미적용 버전
    Former-commit-id: 4ded24d5c2ccde386adcfd9cc3103865d12c2ad7

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d0719bc3af1ed68d4bbbc723f5d1a2aa215980144017d9bfb428ff99fa08f38
-size 7023
+oid sha256:72b4a2a4b0b3bda8300d5b51401f2beaf1465f471df6560d5e7046c6f604b81d
+size 7026
