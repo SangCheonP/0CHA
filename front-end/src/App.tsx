af502ca413c71af9a612839782cef2c0be14691e
JinY <wlsdud6221@naver.com>
2024-08-13 17:48:56 +0900

    fix - ojy - plan test코드로 적용
    Former-commit-id: 121526e33bb5912aa1ef4e05fd53522f2a1df81a

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:daf0ebdf78906446d35670976d872b8458c55c6cf251ce4330948167bef22d09
-size 9172
+oid sha256:1c6db5df22083a2654923bf63f55cf687bba741bd429e83bfacaa3939335dd64
+size 8937
