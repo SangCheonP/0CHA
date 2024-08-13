e6544322a640632975989a341bf40e45fc80ef52
JinY <wlsdud6221@naver.com>
2024-08-13 17:50:18 +0900

    fix - ojy - jwt 문제 수정
    Former-commit-id: 4addabc71420f14242cb8ae91172edbf50547e14

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba24b0e9ec0582f3642262e022b514eabbf3ce9fa5e4e4efeeff4133a55f1fde
-size 3650
+oid sha256:1823cf521668bae3dbaa53c97309aac5109caab9f173b93f354e04a960a22b79
+size 4040
