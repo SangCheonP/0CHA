65a2906889c0484e8e19c759a58a1a8c7554dbe3
JinY <wlsdud6221@naver.com>
2024-08-14 02:07:13 +0900

    fix - ojy - fcm 세팅 값 수정
    Former-commit-id: eaff1d516e46e88580ce600afa068f00d03e9b89

--- front-end/src/firebaseCloudMessaging.js
+++ front-end/src/firebaseCloudMessaging.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ba03814b5132942b1ed8b5f01b35b62052ed89127a5d163d3a1b6b97622d79
-size 1635
+oid sha256:9221926be8c22b8f13a7528a0ca4aeab257747d1c6ef35373d33996d586ff662
+size 1639
