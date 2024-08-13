32d0ec1c10847945b4c89a4d108e853f9e96d714
JinY <wlsdud6221@naver.com>
2024-08-14 08:54:20 +0900

    fix - ojy - fcm 수정
    Former-commit-id: e3d552f20236e44ecd3b32d1e66f723c9a6f519b

--- front-end/src/firebaseCloudMessaging.js
+++ front-end/src/firebaseCloudMessaging.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6026befe34dc979ebd37d33136ae91814a7ae1c4ffd326223ef0ade7f9f417b8
-size 1759
+oid sha256:b4d420252d1f9378bb1ed7ee02b0b02fcb414830dd9c3b99ddf41a934862f63e
+size 1638
