c736dfffbbc1cdd2be2fcdd4c9e93573f2a5219e
Park SangCheon <pswlove38@naver.com>
2024-08-14 23:15:19 +0900

    feat - psc - Add chat config
    Former-commit-id: 33bfbc8e1ffcccd3f6c0fc905a664a12f002fcdd

--- back-end/src/main/java/com/ssafy/back_end/util/FilterChannelInterceptor.java
+++ back-end/src/main/java/com/ssafy/back_end/util/FilterChannelInterceptor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebee8a6814a0efd94bb3d13b3e17a28a3aeeccebf6d90a8e027c0f44c6b61476
-size 2651
+oid sha256:ec4ce5aabb6177ff6596580cfad2e9b52faab463e63a42060ef02b4650306a52
+size 2715
