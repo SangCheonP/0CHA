c0d0bf6416d930f32a0ac947169b05a92eff00ec
JinY <wlsdud6221@naver.com>
2024-08-15 02:11:33 +0900

    fix - ojy - stomp /proxy/ws -> /api/ws
    Former-commit-id: 330c2231b15065e7bc298738680ee3ffa98c943c

--- front-end/src/pages/SocialPage/Feed/ChatPage.tsx
+++ front-end/src/pages/SocialPage/Feed/ChatPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb7fc760202668e0bd3defc127ca43621d16c7a9cbee4a9f785606251a52ccb5
-size 9991
+oid sha256:7693ba58c618558f31dd67d00ab5ed0bdb616a5fec4564b3f67cdb09f900273c
+size 9989
