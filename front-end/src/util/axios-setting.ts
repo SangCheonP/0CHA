9450e82fda873f5115b77f841ec965b6bfc787e1
JinY <wlsdud6221@naver.com>
2024-08-15 04:32:09 +0900

    fix - ojy - FE배포
    Former-commit-id: 0a6c771fc3fc37cfb1f79d476a26f6ac01fe2ae2

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e52e1f7a0af69e526f22972d74a348448045b77aa939be94e2330ff8ea226bc2
-size 10399
+oid sha256:c0b7ed82bdf6bf9b9ba182054109feedc38cd346742855efabf8f1307b593dc9
+size 10486
