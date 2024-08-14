2eb2f02ebe27a6736aba9b0c0d59b59e60f91d02
ANSMOON <munbeumsu@gmail.com>
2024-08-15 03:42:28 +0900

    feat-moon-소셜 로그인 카카오 구현
    Former-commit-id: 9a3322507df2422e3f7680500b452fb25788f4e6

---
+++ back-end/src/main/java/com/ssafy/back_end/oauth/infra/oauth/github/client/GithubApiClient.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20573cd0955ff91d79d574de6a057a19fa8e5ea017dbbf44398c65d3d0809037
+size 1037
---
+++ back-end/src/main/java/com/ssafy/back_end/oauth/infra/oauth/google/dto/GoogleMemberResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5444f34369650c91ae3cb30cb06b55697336ff9d095f86500c2ec6ed370d4659
+size 682
