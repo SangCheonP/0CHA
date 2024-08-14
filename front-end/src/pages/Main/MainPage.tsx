a5f5f576da406bd122adfb3486175b2f46832eb2
JinY <wlsdud6221@naver.com>
2024-08-14 22:27:29 +0900

    fix - ojy - 메인페이지 추천루틴 없을 경우 수정
    Former-commit-id: 39cd0d4c16db004d1d36f442d7ba38f4a77cc273

--- front-end/src/pages/Main/MainPage.tsx
+++ front-end/src/pages/Main/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:970c7df38c0b411ca66b4f673835c8073f40da1b1dc0a0c7a8ca2321c372deef
-size 11374
+oid sha256:0d2663df52c05a8c5995054d9166c554a7f83617cb872b93187b51a7f63cb336
+size 11569
