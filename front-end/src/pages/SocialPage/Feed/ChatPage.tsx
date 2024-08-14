5a79ee370c0c020bd37e687c984b4b755eaf36a9
복현우 <bhwoo1001@naver.com>
2024-08-15 02:34:57 +0900

    feat-bhw2 채팅 완료(스크롤 작업 포함)
    Former-commit-id: fe3a96872041fdfa178343cae08ce6c0aac6a648

--- front-end/src/pages/SocialPage/Feed/ChatPage.tsx
+++ front-end/src/pages/SocialPage/Feed/ChatPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb7fc760202668e0bd3defc127ca43621d16c7a9cbee4a9f785606251a52ccb5
-size 9991
+oid sha256:0fb46cce1378465b1ec0c20d3fc0fcb2432447e32f147dce954dc6f54d5ed147
+size 10908
