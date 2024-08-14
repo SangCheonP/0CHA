070d28e4abc6fe40f9d61a48178e5020a749bbe0
JinY <wlsdud6221@naver.com>
2024-08-14 22:05:51 +0900

    fix - ojy - fitness 엣지케이스 수정
    Former-commit-id: 08398c11cfa469dbe53a506a9d56bf6262873fc5

--- front-end/src/pages/Fitness/FitnessDetailPage.tsx
+++ front-end/src/pages/Fitness/FitnessDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:747929751f7c608b7b51ba711e81017a46ee2f608e3847c7d5102ffa54c4a58f
-size 5314
+oid sha256:f98847d2e3dbdff3412f1d88d7e1e947da6ebf9b5cf6a187e4dcda59ac411734
+size 5308
