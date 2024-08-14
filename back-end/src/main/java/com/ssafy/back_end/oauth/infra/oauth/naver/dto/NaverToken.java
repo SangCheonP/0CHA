e6800d1fbf826d1a8b3baf768659725f073667bb
복현우 <bhwoo1001@naver.com>
2024-08-14 22:58:37 +0900

    feat-bhw2 채팅 작업중
    Former-commit-id: f38450643600c3db7645f2f6a26958cfbe068af0

--- back-end/src/main/java/com/ssafy/back_end/oauth/infra/oauth/naver/dto/NaverToken.java
+++ back-end/src/main/java/com/ssafy/back_end/oauth/infra/oauth/naver/dto/NaverToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c22745b99c8305d4d12ad16c4eee14bfd9be67f805b4b66ab383c80e68ff9f6a
-size 92
+oid sha256:af361e4dd46d5665429953929efd1aaefa4243fef62d36ca6868a8ce0026cec7
+size 88
