9f52369d46aeb9e9e37b8b7817865d9a65e143e9
JinY <wlsdud6221@naver.com>
2024-08-14 16:40:13 +0900

    init - ojy - dev-fe 교체
    Former-commit-id: 8aa95e2b997ea2768db186014b965d233e615145

--- front-end/src/pages/SocialPage/Feed/FeedPage.tsx
+++ front-end/src/pages/SocialPage/Feed/FeedPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:748189e99bb0c1d0782a8e99bba087d83d9a370a9036978206f8222e2110987b
-size 8939
+oid sha256:3d4d4544e5f1598bf6e1b1d497ea05d0f6af4588a25a21c0953abfd5155fa2e2
+size 8980
