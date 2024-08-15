82ac0d0a9fa5954eaa3f9d023855b256ec3ce5b5
JinY <wlsdud6221@naver.com>
2024-08-16 03:41:38 +0900

    fix - ojy - 자잘한 버그 수정
    Former-commit-id: edf3755770891fa0d508330cd6f563f6d781447d

--- back-end/src/main/java/com/ssafy/back_end/oauth/infra/oauth/naver/authcode/NaverAuthCoeRequestUrlProvider.java
+++ back-end/src/main/java/com/ssafy/back_end/oauth/infra/oauth/naver/authcode/NaverAuthCoeRequestUrlProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be982b83fe6f03bb887be89fbc38d84631a56d13835313d57e570fe32226c2a4
-size 110
+oid sha256:d310049245f6a4d7ad8155ee8e86f3be1222a3c4cdffd27d86f93917675fd556
+size 114
---
+++ front-end/src/pages/SocialLoginPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6938edaa3996c89dfef1e996bdf3dec58a0fe698e8bf370647fa4636383ca770
+size 1409
