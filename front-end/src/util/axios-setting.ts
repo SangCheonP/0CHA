ba92d5ea852dfa15378644fc03fcf2065092a6b4
JinY <wlsdud6221@naver.com>
2024-08-16 04:03:08 +0900

    fix - ojy - ocr 프록시 설정 및 소셜 code 수정
    Former-commit-id: d741a21013ce49d96fbdae7fcd77af5698502a05

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d52d9f8deb870a10ecdec1977e2aa305bd061d008a47e4a472ad55e35b69efe
-size 10919
+oid sha256:cb67bb9518bc320672350eaa5418d8abfdf7918032538e47fa89b9488310b78d
+size 10894
